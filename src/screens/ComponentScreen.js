a50adfa533e1d90360f62c5efdd6afebb72a45a9
Sugali Abhishek <sugali.alnaikk.ece17@itbhu.ac.in>
2021-06-12 13:14:46 +0530

    first commit

---
+++ src/screens/ComponentScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+import React from 'react'
+
+const ComponentScreen = ()=>{
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ComponentScreen
+
